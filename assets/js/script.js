11cf6bcf3ddc92515445a1805e92b4d1c9e20352
LongVN99 <LongVn1999vn@yahoo.com>
2020-11-03 15:35:24 +0000

    add store picked card

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,21 @@
+let cardFlipped = false;
+let firstPick, secondPick;
+
 $(document).ready(function () {
   //Run this script only when the document have finished loading
   $(".card").on("click", function () {
-    this.classList.toggle("flip");
+    $(this).addClass("flip");
+
+    if (cardFlipped === false) {
+      //user first click
+      cardFlipped = true;
+      firstPick = this;
+    } else {
+      //user second click
+      cardFlipped = false;
+      secondPick = this;
+    }
+
+    console.log(cardFlipped, firstPick);
   });
 });
