753c7c0414343b3466b8877c6129b7440355e4b5
LongVN99 <LongVn1999vn@yahoo.com>
2020-11-03 14:32:50 +0000

    add flip function (2)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,6 @@
-$(document).ready(function () { //Run this script only when the document have finished loading
+$(document).ready(function () {
+  //Run this script only when the document have finished loading
   $(".card").on("click", function () {
     this.classList.toggle("flip");
-    console.log('I was clicked')
-    console.log(this)
   });
 });
-
-/* Ready function
-function ready() {
-}
-
-function ready() {
-    let overlays = Array.from(document.getElementsByClassName('overlay-text'));
-    let cards = Array.from(document.getElementsByClassName('card'));
-    let game = new MixOrMatch(100, cards);
-
-    overlays.forEach(overlay => {
-        overlay.addEventListener('click', () => {
-            overlay.classList.remove('visible');
-            game.startGame();
-        });
-    });
-    cards.forEach(card => {
-        card.addEventListener('click', () => {
-            game.flipCard(card);
-        });
-    });
-}
-*/
-
-/*
-if (document.readyState === "loading") {
-  document.addEventListener("DOMContentLoaded", ready());
-} else {
-  ready();
-}*/
