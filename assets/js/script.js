48b3b5704deae956e9ffb4abdc3322ea27809ca8
LongVN99 <LongVn1999vn@yahoo.com>
2020-11-09 14:50:06 +0000

    add sound folder and items

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
   //Run this script only when the document have finished loading
   $(".card").on("click", function () {
     $(this).addClass("flip");
-
+    
     if (cardFlipped === false) {
       //user first click
       cardFlipped = true;
