d07f569d995ef601296de44ce9ffcec3faf9f7f5
LongVN99 <LongVn1999vn@yahoo.com>
2020-11-09 16:55:56 +0000

    add lockBoard to script / fix bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,39 @@
 let cardFlipped = false;
 let firstPick, secondPick;
+let lockBoard = false; //you can only interact when lockBoard is false---fix bug
 
 $(document).ready(function () {
   //Run this script only when the document have finished loading
   $(".card").on("click", function () {
-    $(this).addClass("flip");
-    
+    if (lockBoard === false) {
+      $(this).addClass("flip");
+    } else {
+      return;
+    }
+
     if (cardFlipped === false) {
-      //user first click
+      //check if this is first click
       cardFlipped = true;
       firstPick = this;
     } else {
-      //user second click
+      //if not second click
       cardFlipped = false;
       secondPick = this;
+      //Check for match
       if (firstPick.dataset.flag_name === secondPick.dataset.flag_name) {
         // for matched case we will disable the click method so the user can not click on the same card
         $(firstPick).off("click");
         $(secondPick).off("click");
-        //we need this condition to prevent bug
       } else {
         // if it's not match
+        lockBoard = true;
         setTimeout(function () {
-          //setTimeout method allow user to see the second pick
+          //setTimeout method allow user to see the second pick by adding more time before calling the function
           //without this method the game flip the second pick card very fast that the user aren't able to see his second pick
           $(firstPick).removeClass("flip");
           $(secondPick).removeClass("flip");
           //the front card will flip to the back card when we remove the flip class
+          lockBoard = false;
         }, 1000);
       }
     }
