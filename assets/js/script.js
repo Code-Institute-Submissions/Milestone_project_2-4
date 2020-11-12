2e01b16be57b6c1e64284485731e10be13b3d77d
LongVN99 <LongVn1999vn@yahoo.com>
2020-11-12 15:47:30 +0000

    add mute music in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@ let cards = []; // empty array to contain cards
 let cardFlipped = false;
 let firstPick, secondPick;
 let lockBoard = false; //you can only interact when lockBoard is false---fix bug
+var muteMusic = true;
+var MuteSound = true;
 var totalTime = 45;
 
 class AudioController {
@@ -18,10 +20,14 @@ class AudioController {
     this.wrongSound.volume = 0.2; // this sound is quite loud
     this.gameMusic.loop = true;
   }
-  startMusic() {
-    this.gameMusic.play();
+  Music() {
+    if (muteMusic === false) {
+      this.gameMusic.play();
+    } else {
+      this.gameMusic.pause();
+    }
   }
-  startSound() {
+  btnStartSound() {
     this.startGameSound.play();
   }
   stopMusic() {
@@ -44,7 +50,7 @@ class AudioController {
   timeAboutToEnd() {
     this.timeSound.play();
   }
-  gameOver() {
+  gameOverSound() {
     this.stopMusic();
     this.gameOverSound.play();
   }
@@ -64,13 +70,31 @@ function shuffleCards() {
   }
 }
 
-$("btnStart").on("click", function () {
-  let myMusic = new AudioController();
-  myMusic.startMusic();
-  addCards();
+function muteMusic() {
+  $("btnMusic").on("click", function () {
+    if (muteMusic === true) {
+      muteMusic === false;
+    } else {
+      muteMusic === true;
+    }
+  });
+}
 
-  $("time-remaining").replaceWith(timeCountDown);
-});
+function MuteSound() {
+  myMusic.btnStartSound();
+}
+
+function startGame() {
+  $("btnStart").on("click", function () {
+    let myMusic = new AudioController();
+    myMusic.Music();
+
+    /*Check the otherSound here */
+    /////////////////////////
+
+    $("time-remaining").replaceWith(timeCountDown());
+  });
+}
 
 function timeCountDown() {
   var timeRemaining = totalTime;
@@ -80,6 +104,7 @@ function timeCountDown() {
       gameOver();
     }
   }, 1000);
+  return timeRemaining;
 }
 
 function gameOver() {
@@ -87,8 +112,7 @@ function gameOver() {
   alert("you lose");
 }
 
-$(document).ready(function () {
-  //Run this script only when the document have finished loading
+function game() {
   $(".card").on("click", function () {
     if (lockBoard === false) {
       if (this === firstPick) {
@@ -105,32 +129,39 @@ $(document).ready(function () {
     if (cardFlipped === false) {
       cardFlipped = true;
       firstPick = this;
-      myMusic.flip();
+      //myMusic.flip();
     } else {
       //if not second click
       cardFlipped = false;
       secondPick = this;
-      myMusic.flip();
+      //myMusic.flip();
       //Check for match
       if (firstPick.dataset.flag_name === secondPick.dataset.flag_name) {
         // for matched case we will disable the click method so the user can not click on the same card
         $(firstPick).off("click");
         $(secondPick).off("click");
-        myMusic.correct();
+        //myMusic.correct();
       } else {
         // if it's not match
         lockBoard = true;
         setTimeout(function () {
           //setTimeout method allow user to see the second pick by adding more time before calling the function
           //without this method the game flip the second pick card very fast that the user aren't able to see his second pick
-          myMusic.wrong();
+          //myMusic.wrong();
           $(firstPick).removeClass("flip");
           $(secondPick).removeClass("flip");
 
           //the front card will flip to the back card when we remove the flip class
           lockBoard = false;
         }, 1000);
-      }
-    }
+      };
+    };
   });
+};
+
+$(document).ready(function () {
+  addCards();
+  startGame();
+  shuffleCards();
+  game();
 });
