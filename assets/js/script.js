5d40de0e44f074dc1b8559fa42d54cf5ab1e1a2e
LongVN99 <38718117+LongVN99@users.noreply.github.com>
2020-11-16 12:08:36 +0100

    test modal with w3school code

--- assets/js/script.js
+++ assets/js/script.js
@@ -62,7 +62,6 @@ class AudioController {
   }
 }
 
-
 function addCards() {
   $(".card").each(function () {
     cards.push(this);
@@ -87,6 +86,9 @@ function muteMusic() {
     $(".music-status").html('ON')
     musicMuted=true
     playMusic=musicMuted
+    if (playSound===true){
+      myMusic.clickSound();
+    }
   }
 }
 
@@ -112,15 +114,24 @@ function playMusic(){
     muteMusic();
     myMusic.Music();
   });
-}
+};
+$('.btnInstruction').on('click', function(){
+  if (playSound===true){
+    myMusic.clickSound();
+  }
+});
+
 
 function btnStartGame() {
   $("btnStart").on("click", function () {
     /*Check the otherSound here */
     /////////////////////////
+    if (playSound===true){
+      myMusic.btnStartGame();
+    }
     timeCountDown();
   });
-}
+};
 
 function timeCountDown() {
   timeRemaining = totalTime;
@@ -200,3 +211,29 @@ $(document).ready(function () {
   shuffleCards();
   game();
 });
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
